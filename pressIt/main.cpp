18794125fbb195df96a5dd4108e2e16485f4805a
Alfredo Liardo <alfredoliardo@MacBook-Pro-di-Alfredo.local>
2015-02-21 15:15:50 +0100

    Initial Commit

---
+++ pressIt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pressIt
+//
+//  Created by Alfredo Liardo on 21/02/15.
+//  Copyright (c) 2015 Alfredo Liardo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
